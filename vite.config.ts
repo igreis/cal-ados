8487123bf5778fe06fe77e45aa38ec6c4f6301b2
igor <igalvesreis2121@gmail.com>
2025-04-02 14:52:29 -0300

    gh-pages adicionado

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cal-ados',  // Adicione o nome do seu repositório GitHub
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
